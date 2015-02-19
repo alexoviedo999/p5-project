67c29c467c60bc54ceaf525e830ec6a319deef92
alexoviedo <alexoviedo999@gmail.com>
2015-02-19 03:01:46 -0500

    nunchuck changes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ console.log("http server listening on %d", port);
 app.use(express.static(__dirname + '/public'));
 
 // Sockets
-require('public/lib/nunchuck-server.js')(io);
+require('server/nunchuck-server.js')(io);
 
 // Detect if mobile
 app.use(function(req, res, next){
