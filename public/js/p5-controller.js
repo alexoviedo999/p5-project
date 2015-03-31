607c53ff0c4181d0db887419d9997ef94a8a6551
alexoviedo <alexoviedo999@gmail.com>
2015-03-31 17:14:34 -0400

    testing controller orientation lock.

--- public/js/p5-controller.js
+++ public/js/p5-controller.js
@@ -3,7 +3,7 @@
 
   var sketch = function(s){
     s.setup = function() {
-      tp = s.createCanvas(s.windowWidth, s.windowHeight*0.8);
+      tp = s.createCanvas(s.windowWidth, s.windowHeight*0.5);
       tp.parent('touch-pad');
       timeSlider = s.createSlider(1, 5, 3);
       timeSlider.parent('knob-pad');
@@ -32,5 +32,33 @@
   */
   containerNode = document.getElementById( 'canvas' );
   myp5Ctrl = new p5(sketch, containerNode);
+
+  $(window).bind('orientationchange resize', function(event){
+    if (event.orientation) {
+      if (event.orientation == 'landscape') {
+        if (window.rotation == 90) {
+          rotate(this, -90);
+        } else {
+          rotate(this, 90);
+        }
+      }
+    }
+  });
+
+  function rotate(el, degs) {
+    iedegs = degs/90;
+    if (iedegs < 0) iedegs += 4;
+    transform = 'rotate('+degs+'deg)';
+    iefilter = 'progid:DXImageTransform.Microsoft.BasicImage(rotation='+iedegs+')';
+    styles = {
+      transform: transform,
+      '-webkit-transform': transform,
+      '-moz-transform': transform,
+      '-o-transform': transform,
+      filter: iefilter,
+      '-ms-filter': iefilter
+    };
+    $(el).css(styles);
+  }
 
 }
